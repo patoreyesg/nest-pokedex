843af9fb6b7664b28fa505dea1c8cb2eac6c695f
Patricio Reyes G <preyes@cydingenieria.cl>
2024-04-13 21:42:19 -0400

    [FEAT] nuevo modulo
         Se agrega nuevo modulo common

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -12,6 +13,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
 
     PokemonModule,
+
+    CommonModule,
   ],
   controllers: [],
   providers: [],
